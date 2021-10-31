fe5c7176868e6035699afb32ad501d10a673e36f
luis-castaneda7 <luis_casta7@outlook.com>
2021-10-31 15:05:46 -0500

    terminal shortcut

--- config.h
+++ config.h
@@ -45,6 +45,7 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod4Mask
+#define ALT Mod1Mask
 #define CTRL ControlMask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
@@ -65,6 +66,7 @@ static const char *termcmd[]  = { "st", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ CTRL,                         XK_space,  spawn,          {.v = dmenucmd } },
+	{ CTRL|ALT,                     XK_t,      spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
