41b4a6b2348bcbd3a9ea70f1948aea526454bb19
luis-casta7 <luis_casta7@outlook.com>
2021-11-08 22:15:42 -0600

    added scratchpad

--- config.h
+++ config.h
@@ -62,11 +62,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x30", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ CTRL,                         XK_space,  spawn,          {.v = dmenucmd } },
-	{ CTRL|ALT,                     XK_t,      spawn,          {.v = termcmd } },
+	{ CTRL|ALT,                     XK_t,  	   togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ CTRL,                         XK_Tab,    focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
