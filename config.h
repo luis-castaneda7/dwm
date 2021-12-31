2b8d5a5a26d6a4df49b7001ab3794665e42a302f
luis-casta7 <luis_casta7@outlook.com>
2021-12-31 05:25:59 -0600

    volume control

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono-Regular:size=15:antialias=true:autohint=true" };
-static const char dmenufont[]       = "monospace:size=15";
+static const char *fonts[]          = { "JetBrainsMono-Regular:size=12:antialias=true:autohint=true" };
+static const char dmenufont[]       = "JetBrainsMono-Regular:size=12:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -78,7 +78,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ CTRL,		                	XK_q,      killclient,     {0} },
+	{ CTRL,		                XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_f,  	   setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -88,8 +88,11 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ CTRL|ShiftMask,               XK_4,      spawn,          		  SHCMD("screenshot") },
-	{ 0,               		XF86XK_MonBrightnessUp,   spawn,          SHCMD("light -A 5") },
-	{ 0,               		XF86XK_MonBrightnessDown, spawn,          SHCMD("light -U 5") },
+	{ 0,               		XF86XK_MonBrightnessUp,   spawn,          SHCMD("light -A 3") },
+	{ 0,               		XF86XK_MonBrightnessDown, spawn,          SHCMD("light -U 3") },
+	{ 0,               		XF86XK_AudioMute, 	  spawn,          SHCMD("amixer set toggle") },
+	{ 0,               		XF86XK_AudioRaiseVolume,  spawn,          SHCMD("amixer set Master 5%+") },
+	{ 0,               		XF86XK_AudioLowerVolume,  spawn,          SHCMD("amixer set Master 5%-") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
